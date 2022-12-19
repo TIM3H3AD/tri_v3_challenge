86a0dc2345f70f0aecf16619f96954ededd1e814
TIM3H3AD <whalebone.earth@gmail.com>
2022-12-18 20:13:05 -0500

    new seednode added 4kzmcfgutwkihzww.onion PINBALL(wv) 2186966

---
+++ src/obj/build.h
@@ -0,0 +1,2 @@
+// No build information available
+#define BUILD_DATE "2022-12-18 15:44:44 -0500"
--- src/onionseed.h
+++ src/onionseed.h
@@ -4,12 +4,12 @@
 
 // hidden service seeds
 static const char *strMainNetOnionSeed[][1] = {
-    {"whaleqt5h3e4b5olfqv3p3zyctgn3lur2ppsz7dtfcwitez4ksiw6bad.onion"},
+    {"4kzmcfgutwkihzww.onion"},
     {NULL}
 };
 
 static const char *strTestNetOnionSeed[][1] = {
-    {"whaleqt5h3e4b5olfqv3p3zyctgn3lur2ppsz7dtfcwitez4ksiw6bad.onion"},
+    {"4kzmcfgutwkihzww.onion"},
     {NULL}
 };
 
